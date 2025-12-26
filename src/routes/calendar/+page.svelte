506e702825884b3634d3241a952de9cef295d0d4
hubsch <hubize@gmail.com>
2025-12-26 21:00:50 +0100

    calendar and statistics components

---
+++ src/routes/calendar/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import '$lib/styles.css';
+</script>
+
+<div id="header">
+	<h1>Fitolongo - Calendar</h1>
+</div>
+<ul>
+	<li><a href="/calendar">Calendar</a></li>
+	<li><a href="/statistics">Statistics</a></li>
+	<li><a href="/Notes">Notes</a></li>
+</ul>
