835c1410782961be0eeaef2a55417578c3babff6
hubsch <hubize@gmail.com>
2025-12-26 18:58:49 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,38 @@
+<div id="header">
+	<h1>Fitolongo Calendar</h1>
+</div>
+<ul>
+	<li><a href="/calendar">Calendar</a></li>
+	<li><a href="/statistics">Statistics</a></li>
+	<li><a href="/Notes">Notes</a></li>
+</ul>
+
+<style>
+	#header {
+		background-color: #d9d9d9;
+		width: 100%;
+		padding: 0.015rem 0;
+	}
+
+	h1 {
+		text-align: center;
+	}
+
+	ul {
+		display: flex;
+		list-style-type: none;
+		margin: 0;
+		padding: 0;
+		/* overflow: hidden; */
+		background-color: #d9d9d9;
+		justify-content: space-between;
+		font-size: 1.2rem;
+	}
+
+	li {
+		margin: 0 275px;
+		background-color: #a7a7a7;
+		padding: 10px 30px;
+		/* margin-left: 0px; */
+	}
+</style>
